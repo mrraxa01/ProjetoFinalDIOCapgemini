a4787a26d2e3be1187631ab9a516c9a06696e756
Marcio Rodrigues <mrraxa01@gmail.com>
2021-09-26 22:37:43 -0300

    Estilizando o footer

--- frontend/blog/src/app/footer/footer.component.ts
+++ frontend/blog/src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faInstagram, faLinkedin, faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  faInstagram = faInstagram
+  faLinkedin = faLinkedin
+  faFacebook = faFacebook
   constructor() { }
 
   ngOnInit(): void {
