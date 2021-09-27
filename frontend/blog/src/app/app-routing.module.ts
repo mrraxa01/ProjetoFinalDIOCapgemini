32a35365d3287454e83e1fd492e98f49f33f77c2
Marcio Rodrigues <mrraxa01@gmail.com>
2021-09-26 21:51:47 -0300

    adicionado bootstrap e fontawesome, criado componentes footer, navbar, feed, contato, e personalizado navbar

--- frontend/blog/src/app/app-routing.module.ts
+++ frontend/blog/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
 import { FeedComponent } from './feed/feed.component';
 import { HomeComponent } from './home/home.component';
 
@@ -8,7 +9,8 @@ const routes: Routes = [
   {
     path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
-    {path: 'feed', component: FeedComponent}
+    {path: 'feed', component: FeedComponent},
+    {path: 'contato', component: ContatoComponent}
   
 ];
 
